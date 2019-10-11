9b8a9bdf6174ef4303e4d58e3daa0526c0c6f1b5
lvbu <lvbu@qq.com>
2019-10-11 10:58:50 +0800

    第一次创建的Test_1

---
+++ src/main/java/com/bw/bean/Test_1.java
@@ -0,0 +1,15 @@
+package com.bw.bean;
+
+import org.junit.Test;
+
+public class Test_1 {
+
+	public static void main(String[] args) {
+		System.out.println("HelloWord~!");
+	}
+	
+	@Test
+	public void show(){
+		System.out.println("HelloWord~!");
+	}
+}
